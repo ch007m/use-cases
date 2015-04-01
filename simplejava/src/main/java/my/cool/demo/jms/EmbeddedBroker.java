a75feb611f4b393b2d85b0fd36fdf11b17b5ff40
Charles Moulliard <ch007m@gmail.com>
2015-04-01 18:38:38 +0200

    Add jms embedded broker and publisher

---
+++ simplejava/src/main/java/my/cool/demo/jms/EmbeddedBroker.java
@@ -0,0 +1,26 @@
+package my.cool.demo.jms;
+
+import org.apache.activemq.broker.BrokerService;
+
+/**
+ * This example demonstrates how to run an embedded broker inside your Java code
+ * 
+ * 
+ */
+public final class EmbeddedBroker {
+
+    private EmbeddedBroker() {}
+
+    public static void main(String[] args) throws Exception {
+        BrokerService broker = new BrokerService();
+        broker.setUseJmx(true);
+        broker.addConnector("tcp://localhost:61616");
+        broker.start();
+
+        // now lets wait forever to avoid the JVM terminating immediately
+        Object lock = new Object();
+        synchronized (lock) {
+            lock.wait();
+        }
+    }
+}
---
+++ simplejava/src/main/java/my/cool/demo/jms/Publisher.java
@@ -0,0 +1,86 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package my.cool.demo.jms;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+
+class Publisher {
+    
+    private static final Logger logger = LoggerFactory.getLogger(Publisher.class);
+
+    public static void main(String []args) throws JMSException {
+
+        String user = env("ACTIVEMQ_USER", "");
+        String password = env("ACTIVEMQ_PASSWORD", "");
+        String host = env("ACTIVEMQ_HOST", "localhost");
+        int port = Integer.parseInt(env("ACTIVEMQ_PORT", "61616"));
+        String destination = arg(args, 0, "foo.bar");
+
+        int messages = 100;
+        int size = 256;
+
+        String DATA = "abcdefghijklmnopqrstuvwxyz";
+        String body = "";
+        for( int i=0; i < size; i ++) {
+            body += DATA.charAt(i%DATA.length());
+        }
+
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://" + host + ":" + port);
+
+        Connection connection = factory.createConnection(user, password);
+        logger.info("Connection created");
+        connection.start();
+        logger.info("Connection started");
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination dest = new ActiveMQQueue(destination);
+        MessageProducer producer = session.createProducer(dest);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        for( int i=1; i <= messages; i ++) {
+            TextMessage msg = session.createTextMessage(body);
+            msg.setIntProperty("id", i);
+            producer.send(msg);
+            logger.info("Message send" + i );
+        }
+
+        producer.send(session.createTextMessage("THE.END"));
+        connection.close();
+        logger.info("Connection closed");
+
+    }
+
+    private static String env(String key, String defaultValue) {
+        String rc = System.getenv(key);
+        if( rc== null )
+            return defaultValue;
+        return rc;
+    }
+
+    private static String arg(String []args, int index, String defaultValue) {
+        if( index < args.length )
+            return args[index];
+        else
+            return defaultValue;
+    }
+
+}
