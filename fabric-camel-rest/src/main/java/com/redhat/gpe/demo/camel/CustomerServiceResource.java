891da389d2f92e0ede90d261199b4c079f7fd197
Charles Moulliard <ch007m@gmail.com>
2015-06-08 18:43:15 +0200

    Finished to document the project

--- fabric-camel-rest/src/main/java/com/redhat/gpe/demo/camel/CustomerServiceResource.java
+++ fabric-camel-rest/src/main/java/com/redhat/gpe/demo/camel/CustomerServiceResource.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,24 +19,17 @@
 package com.redhat.gpe.demo.camel;
 
 import com.redhat.gpe.demo.camel.model.Customer;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
+import com.wordnik.swagger.annotations.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("/customerservice/")
+@Api(value = "/customerservice", description = "Operations about customerservice")
 public class CustomerServiceResource {
 
-    public CustomerServiceResource() {}
-
-/*    @GET
-    @Path("/customers/{id}/")
-    public Response getCustomer(@PathParam("id") String id) {
-        return null;
-    }*/
+    public CustomerServiceResource() {
+    }
 
     @GET
     @Path("/customers/{id}/")
@@ -45,23 +38,39 @@ public class CustomerServiceResource {
             @ApiResponse(code = 500, message = "Invalid ID supplied"),
             @ApiResponse(code = 204, message = "Customer not found")
     })
-    public Response getCustomer(@ApiParam(value = "ID of Customer to fetch", required = true) @PathParam("id") String id) {return null; }
+    public Response getCustomer(@ApiParam(value = "ID of Customer to fetch", required = true) @PathParam("id") String id) {
+        return null;
+    }
 
     @PUT
     @Path("/customers/")
     @Consumes({"application/xml", "application/json"})
-    public Response updateCustomer(Customer customer) {
+    @ApiOperation(value = "Update an existing Customer")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Invalid ID supplied"),
+            @ApiResponse(code = 204, message = "Customer not found")
+    })
+    public Response updateCustomer(@ApiParam(value = "Customer object that needs to be updated", required = true) Customer customer) {
         return null;
     }
 
     @POST
     @Path("/customers/")
     @Consumes({"application/xml", "application/json"})
-    public Response addCustomer(Customer customer) {
+    @ApiOperation(value = "Add a new Customer")
+    @ApiResponses(value = {@ApiResponse(code = 500, message = "Invalid ID supplied"),})
+    public Response addCustomer(@ApiParam(value = "Customer object that needs to be updated", required = true) Customer customer) {
         return null;
     }
 
     @DELETE
     @Path("/customers/{id}/")
-    public Response deleteCustomer(@PathParam("id") String id) { return null; }
+    @ApiOperation(value = "Delete Customer")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Invalid ID supplied"),
+            @ApiResponse(code = 204, message = "Customer not found")
+    })
+    public Response deleteCustomer(@ApiParam(value = "ID of Customer to delete", required = true) @PathParam("id") String id) {
+        return null;
+    }
 }
