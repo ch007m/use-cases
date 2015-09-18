56eb4cd8f1d36e9563674e14ec26324038477f96
Charles Moulliard <ch007m@gmail.com>
2015-09-18 17:01:04 +0200

    Add more info

--- logging-collector/src/main/java/my/cool/logging/LoggingService.java
+++ logging-collector/src/main/java/my/cool/logging/LoggingService.java
@@ -6,12 +6,8 @@ import org.ops4j.pax.logging.spi.PaxLoggingEvent;
 import org.osgi.service.log.LogReaderService;
 import org.osgi.service.log.LogService;
 
-@Component(description = "My Logging Service", immediate = true)
-@Service(PaxAppender.class)
-@Properties({
-        @Property(name = "org.ops4j.pax.logging.appender.name", value = "cool")
-})
-public class LoggingService implements PaxAppender {
+@Component(description = "My OSGI Logging Service", immediate = true)
+public class LoggingService {
 
     @Reference(referenceInterface = LogService.class)
     LogService LOGGER;
@@ -29,31 +25,12 @@ public class LoggingService implements PaxAppender {
     @Activate
     public void activate() {
         LOGGER.log(INFO, "Logging Service started");
-        //reader.addLogListener(m_console);
+        reader.addLogListener(m_console);
     }
 
     @Deactivate
     public void deactivate() {
         LOGGER.log(INFO, "Logging Service stopped");
-        //reader.removeLogListener(m_console);
-    }
-
-    @Override
-    public void doAppend(final PaxLoggingEvent log) {
-        StringBuffer str = new StringBuffer();
-
-        str.append("APPENDER :: ");
-        str.append(LogLevel.getLogName(3) + ": '" + log.getMessage() + "')");
-        str.append(" - Local info : ");
-        str.append(log.getLocationInformation());
-        if (log.getThrowableStrRep() != null) {
-            str.append("\n");
-            String[] stack = log.getThrowableStrRep();
-            for (String traceElement : stack) {
-                str.append("\n");
-                str.append("        " + traceElement);
-            }
-        }
-        System.out.println(str.toString());
+        reader.removeLogListener(m_console);
     }
 }
