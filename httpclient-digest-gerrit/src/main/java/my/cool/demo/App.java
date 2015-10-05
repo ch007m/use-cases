28e49aa2b7125613e68ab43a5698ed6c747f8094
Charles Moulliard <ch007m@gmail.com>
2015-10-05 08:35:33 +0200

    yeh yeh. Code is working

--- httpclient-digest-gerrit/src/main/java/my/cool/demo/App.java
+++ httpclient-digest-gerrit/src/main/java/my/cool/demo/App.java
@@ -2,29 +2,17 @@ package my.cool.demo;
 
 import org.apache.http.*;
 import org.apache.http.auth.*;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.ClientContext;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.DigestScheme;
-import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class App {
 
@@ -32,93 +20,11 @@ public class App {
 
         Logger LOG = LoggerFactory.getLogger(App.class);
 
-        /*String GERRIT_URL = "http://localhost:8080/a/projects/demo22";
-        HttpClient httpclient = new DefaultHttpClient();
-
-        HttpPost post = new HttpPost(GERRIT_URL);
-
-        StringEntity entity = new StringEntity("string");
-        entity.setContentType("application/json");
-        post.setEntity(entity);
-
-        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-        nvps.add(new BasicNameValuePair("username", "admin"));
-        nvps.add(new BasicNameValuePair("password", "secret"));
-        post.setEntity(new UrlEncodedFormEntity(nvps));
-
-        DigestScheme digestAuth = new DigestScheme();
-        digestAuth.overrideParamter("algorithm", "MD5");
-        digestAuth.overrideParamter("uri", "/a/projects/dabou34");
-
-        digestAuth.overrideParamter("realm", "Gerrit Code Review");
-        digestAuth.overrideParamter("nonce", Long.toString(new Random().nextLong(), 36));
-        digestAuth.overrideParamter("qop", "auth");
-        digestAuth.overrideParamter("nc", "0");
-        digestAuth.overrideParamter("cnonce", DigestScheme.createCnonce());
-        digestAuth.overrideParamter("opaque","ba897c2f0f3de9c6f52d");
-
-        try {
-            digestAuth.processChallenge(null);
-            //force  qop in use challenge  on return header ????!!!!
-        } catch (Exception e) {
-            System.out.println("Error : " + e);
-        }
-
-        Header auth = digestAuth.authenticate(new
-                UsernamePasswordCredentials("admin", "secret"), post);
-        post.setHeader(auth);
-
-        HttpResponse response = httpclient.execute(post);
-
-        String content = EntityUtils.toString(response.getEntity());
-        String status = Integer.toString(response.getStatusLine().getStatusCode());
-
-        LOG.info("\nHTTP Status " + status + "\nMessage : " + content);
-*/
-/*        HttpHost targetHost = new HttpHost("localhost", 8080, "http");
-        DefaultHttpClient httpclient = new DefaultHttpClient();
-
-        final String userName = "admin";
-        final String password = "secret";
-
-        httpclient.getCredentialsProvider().setCredentials(
-                new AuthScope("localhost", 8080),
-                new UsernamePasswordCredentials(userName, password));
-
-        // Create AuthCache instance
-        AuthCache authCache = new BasicAuthCache();
-        // Generate DIGEST scheme object, initialize it and add it to the local
-        // auth cache
-        DigestScheme digestAuth = new DigestScheme();
-        // Suppose we already know the realm name
-        digestAuth.overrideParamter("realm", "Gerrit Code Review");
-        // Suppose we already know the expected nonce value
-        digestAuth.overrideParamter("nonce", Long.toString(new Random().nextLong(), 36));
-        authCache.put(targetHost, digestAuth);
-
-        // Add AuthCache to the execution context
-        BasicHttpContext localcontext = new BasicHttpContext();
-        localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
-
-        HttpGet httpget = new HttpGet("http://localhost:8080/a/projects/demo");
-
-        try {
-            HttpResponse response = httpclient.execute(targetHost, httpget, localcontext);
-            HttpEntity entity = response.getEntity();
-
-            System.out.println("----------------------------------------");
-            System.out.println(response.getStatusLine());
-            if (entity != null) {
-                System.out.println("Response content length: " + entity.getContentLength());
-            }
-            EntityUtils.consume(entity);
-        } finally {
-            httpclient.getConnectionManager().shutdown();
-        }*/
+        String GERRIT_URL = "http://localhost:8080/a/projects/demo46";
 
         DefaultHttpClient httpclient = new DefaultHttpClient();
-        DefaultHttpClient httpclient2 = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet("http://localhost:8080/a/projects/demo44");
+        DefaultHttpClient httpclientPost = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(GERRIT_URL);
         System.out.println("Requesting : " + httpget.getURI());
 
         try {
@@ -142,8 +48,11 @@ public class App {
                 UsernamePasswordCredentials creds = new UsernamePasswordCredentials("admin", "secret");
                 httpget.addHeader(digestScheme.authenticate(creds, httpget));
 
+                HttpPost httpPost = new HttpPost(GERRIT_URL);
+                httpPost.addHeader(digestScheme.authenticate(creds, httpPost));
+
                 ResponseHandler<String> responseHandler = new BasicResponseHandler();
-                String responseBody = httpclient2.execute(httpget, responseHandler);
+                String responseBody = httpclientPost.execute(httpPost, responseHandler);
                 System.out.println("responseBody : " + responseBody);
             }
 
@@ -155,9 +64,7 @@ public class App {
             System.out.println("Response from Gerrit Server : " + e.getMessage());
         } finally {
             httpclient.getConnectionManager().shutdown();
-            httpclient2.getConnectionManager().shutdown();
+            httpclientPost.getConnectionManager().shutdown();
         }
-
-
     }
 }
