c07b414ae61401983efc3121c595eedf84b62e67
Charles Moulliard <cmoulliard@apache.org>
2016-03-04 14:01:51 +0100

    Update test case to use a list

--- demo-camel-drools-kie-spring/src/test/java/org/acme/test/KieGlobalsTest.java
+++ demo-camel-drools-kie-spring/src/test/java/org/acme/test/KieGlobalsTest.java
@@ -1,5 +1,6 @@
 package org.acme.test;
 
+import org.acme.model.Person;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kie.api.runtime.KieSession;
@@ -9,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -28,8 +30,16 @@ public class KieGlobalsTest {
         KieSession ksession = (KieSession) context.getBean("ksession1");
         assertNotNull(ksession);
 
+        // Key = persons
         Collection col = ksession.getGlobals().getGlobalKeys();
-        assertEquals(2, col.size());
+        assertEquals(1, col.size());
+
+        // List contains 2 records
+        List<Person> persons = (List<Person>) ksession.getGlobals().get("persons");
+        assertNotNull(persons);
+        assertEquals("charles",persons.get(0).getName());
+        assertEquals("samuel",persons.get(1).getName());
+
 
     }
 
