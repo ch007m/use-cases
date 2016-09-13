b8aa1e483c5bd427ce02f90f2e476da61f864be5
Charles Moulliard <cmoulliard@apache.org>
2016-09-13 16:08:07 +0200

    Add vertx project

---
+++ vertx-kube-configmap/README.md
@@ -0,0 +1,41 @@
+# Instructions
+
+* Launch minishift
+
+    minishift start
+    minishit docker-env
+    
+* Setup the Kubernetes ENV variable required on your machine 
+
+    export KUBERNETES_MASTER=https://192.168.64.11:8443
+    
+* Log to openshift
+    
+    oc login 192.168.64.11:8443 -u admin -p admin
+    
+# Create a new project
+    
+    oc new-project vertx-demo
+    oc policy add-role-to-user view openshift-dev -n vertx-demo
+    oc policy add-role-to-group view system:serviceaccounts -n vertx-demo
+    
+# Create the ConfigMap
+
+    oc create configmap game-config --from-literal=key=some.properties
+    oc create configmap from-json --from-literal=key=file-json.json
+    oc create configmap special-config    
+
+* To compile
+
+    mvn clean install
+    
+* To build the docker image and generate the Kubernetes config file
+
+    mvn -Pf8-build -Popenshift
+    
+* To deploy on OpenShift 
+    
+    mvn -Pf8-local-deploy -Popenshift    
+
+# Deploy project
+
---
+++ vertx-kube-configmap/src/main/java/io/vertx/example/kubernetes/MyVerticle.java
@@ -0,0 +1,48 @@
+package io.vertx.example.kubernetes;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.configuration.ConfigurationService;
+import io.vertx.ext.configuration.ConfigurationServiceOptions;
+import io.vertx.ext.configuration.ConfigurationStoreOptions;
+
+public class MyVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        ConfigurationStoreOptions store1 = new ConfigurationStoreOptions();
+        store1.setType("configmap")
+                .setFormat("properties")
+                .setConfig(new JsonObject()
+                        .put("name", "game-config")
+                        .put("key", "some.properties"));
+
+        ConfigurationStoreOptions store3 = new ConfigurationStoreOptions();
+        store3.setType("configmap")
+                .setConfig(new JsonObject()
+                        .put("name", "from-json")
+                        .put("key", "file-json.json"));
+
+        ConfigurationStoreOptions store2 = new ConfigurationStoreOptions();
+        store2.setType("configmap")
+                .setConfig(new JsonObject()
+                        .put("name", "special-config"));
+
+
+        ConfigurationService conf = ConfigurationService.create(vertx, new ConfigurationServiceOptions()
+                .addStore(store1).addStore(store2).addStore(store3));
+
+        conf.getConfiguration(ar -> {
+            if (ar.failed()) {
+                ar.cause().printStackTrace();
+            } else {
+                System.out.println(ar.result().encodePrettily());
+            }
+        });
+
+        conf.listen(newConfiguration -> {
+            System.out.println("New configuration: " + newConfiguration.encodePrettily());
+        });
+    }
+
+}
